87524d75f4550e1a0bf7cca2f602ad240500f730
Abdallahk222 <118539603+Abdallahk222@users.noreply.github.com>
2023-11-30 22:05:36 +0100

    fix : session and connection buttons

--- client/pages/_app.jsx
+++ client/pages/_app.jsx
@@ -1,27 +1,51 @@
 import Link from "next/link";
 import "../styles/globals.css";
+import { createClient } from "@supabase/supabase-js";
 import { createPagesBrowserClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
+
 export default function App({ Component, pageProps }) {
   const router = useRouter();
   const [supabaseClient] = useState(() => createPagesBrowserClient());
-  const [darkMode, setDarkMode] = useState(false); 
+  const [darkMode, setDarkMode] = useState(false);
   const isActive = (path) => router.pathname === path;
+  const [session, setSession] = useState(null);
 
   useEffect(() => {
     const html = document.documentElement;
     if (darkMode) {
-      html.classList.add('dark');
+      html.classList.add("dark");
     } else {
-      html.classList.remove('dark');
+      html.classList.remove("dark");
     }
   }, [darkMode]);
 
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
+  };
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+    return () => subscription;
+  }, [setSession]);
+
+  const SignOut = () => {
+    supabase.auth.signOut().then(() => {
+      router.push("/login");
+    });
   };
 
   return (
@@ -33,91 +57,129 @@ export default function App({ Component, pageProps }) {
         <h1 className="font-bold text-4xl text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-blue-600">
           WhatFilm
         </h1>
+        <button
+          onClick={toggleDarkMode}
+          className={`fixed top-5 left-5 py-2 px-4 rounded-lg shadow-lg transition duration-300 ease-in-out ${
+            darkMode
+              ? "bg-gradient-to-r from-cyan-400 to-blue-600 text-white"
+              : "bg-gradient-to-r from-gray-200 to-gray-300 text-black"
+          }`}
+        >
+          {darkMode ? "Mode Lumineux" : "Mode Sombre"}
+        </button>
+        {!session ? (
+          <button>
+            <Link
+              href="/login"
+              className={`fixed top-5 right-5 ${
+                isActive("/login")
+                  ? "bg-gradient-to-r from-cyan-400 to-blue-600 text-white py-2 px-4 rounded-lg shadow-lg"
+                  : "bg-gray-200 text-black py-2 px-4 rounded-lg hover:bg-gray-300"
+              }`}
+            >
+              Login
+            </Link>
+          </button>
+        ) : (
+          <>
+            <button>
+              <Link
+                href="/profile"
+                className={`fixed top-5 right-5 ${
+                  isActive("/profile")
+                    ? "bg-gradient-to-r from-cyan-400 to-blue-600 text-white py-2 px-4 rounded-lg shadow-lg"
+                    : "bg-gray-200 text-black py-2 px-4 rounded-lg hover:bg-gray-300"
+                }`}
+              >
+                Profile
+              </Link>
+            </button>
+            <button
+              onClick={SignOut}
+              className="fixed top-5 right-40 bg-gray-200 text-black py-2 px-4 rounded-lg hover:bg-gray-300"
+            >
+              Log Out
+            </button>
+          </>
+        )}
       </header>
+      <br></br>
       <nav className="flex justify-center mt-12">
         <ul className="flex justify-between space-x-10">
-        <li>
-        <Link href="/"
-            className={
-              isActive("/")
-                ? "bg-gradient-to-r from-cyan-400 to-blue-600 text-white py-2 px-4 rounded-lg shadow-lg"
-                : "bg-gray-200 text-black py-2 px-4 rounded-lg hover:bg-gray-300"
-            }
-          >
-            Home</Link>
+          <li>
+            <Link
+              href="/"
+              className={
+                isActive("/")
+                  ? "bg-gradient-to-r from-cyan-400 to-blue-600 text-white py-2 px-4 rounded-lg shadow-lg"
+                  : "bg-gray-200 text-black py-2 px-4 rounded-lg hover:bg-gray-300"
+              }
+            >
+              Home
+            </Link>
           </li>
           <li>
-          <Link href="/about"
-            className={
-              isActive("/about")
-                ? "bg-gradient-to-r from-cyan-400 to-blue-600 text-white py-2 px-4 rounded-lg shadow-lg"
-                : "bg-gray-200 text-black py-2 px-4 rounded-lg hover:bg-gray-300"
-            }
-          >
-            About Us</Link>
+            <Link
+              href="/about"
+              className={
+                isActive("/about")
+                  ? "bg-gradient-to-r from-cyan-400 to-blue-600 text-white py-2 px-4 rounded-lg shadow-lg"
+                  : "bg-gray-200 text-black py-2 px-4 rounded-lg hover:bg-gray-300"
+              }
+            >
+              About Us
+            </Link>
           </li>
           <li>
-          <Link href="/contacts"
-            className={
-              isActive("/contacts")
-                ? "bg-gradient-to-r from-cyan-400 to-blue-600 text-white py-2 px-4 rounded-lg shadow-lg"
-                : "bg-gray-200 text-black py-2 px-4 rounded-lg hover:bg-gray-300"
-            }
-          >
-            Contact Us</Link>
+            <Link
+              href="/contacts"
+              className={
+                isActive("/contacts")
+                  ? "bg-gradient-to-r from-cyan-400 to-blue-600 text-white py-2 px-4 rounded-lg shadow-lg"
+                  : "bg-gray-200 text-black py-2 px-4 rounded-lg hover:bg-gray-300"
+              }
+            >
+              Contact Us
+            </Link>
           </li>
           <li>
-          <Link href="/articles"
-            className={
-              isActive("/articles")
-                ? "bg-gradient-to-r from-cyan-400 to-blue-600 text-white py-2 px-4 rounded-lg shadow-lg"
-                : "bg-gray-200 text-black py-2 px-4 rounded-lg hover:bg-gray-300"
-            }
-          >
-            Articles</Link>
+            <Link
+              href="/articles"
+              className={
+                isActive("/articles")
+                  ? "bg-gradient-to-r from-cyan-400 to-blue-600 text-white py-2 px-4 rounded-lg shadow-lg"
+                  : "bg-gray-200 text-black py-2 px-4 rounded-lg hover:bg-gray-300"
+              }
+            >
+              Articles
+            </Link>
           </li>
           <li>
-          <Link href="/use-state"
-            className={
-              isActive("/use-state")
-                ? "bg-gradient-to-r from-cyan-400 to-blue-600 text-white py-2 px-4 rounded-lg shadow-lg"
-                : "bg-gray-200 text-black py-2 px-4 rounded-lg hover:bg-gray-300"
-            }
-          >
-            Use state</Link>
+            <Link
+              href="/use-state"
+              className={
+                isActive("/use-state")
+                  ? "bg-gradient-to-r from-cyan-400 to-blue-600 text-white py-2 px-4 rounded-lg shadow-lg"
+                  : "bg-gray-200 text-black py-2 px-4 rounded-lg hover:bg-gray-300"
+              }
+            >
+              Use state
+            </Link>
           </li>
           <li>
-          <Link href="/login"
-            className={
-              isActive("/login")
-                ? "bg-gradient-to-r from-cyan-400 to-blue-600 text-white py-2 px-4 rounded-lg shadow-lg"
-                : "bg-gray-200 text-black py-2 px-4 rounded-lg hover:bg-gray-300"
-            }
-          >
-            Login</Link>
-          </li>
-          <li>
-          <Link href="/admin/contacts"
-            className={
-              isActive("/admin/contacts")
-                ? "bg-gradient-to-r from-cyan-400 to-blue-600 text-white py-2 px-4 rounded-lg shadow-lg"
-                : "bg-gray-200 text-black py-2 px-4 rounded-lg hover:bg-gray-300"
-            }
-          >
-            Admin (contacts)</Link>
+            <Link
+              href="/admin/contacts"
+              className={
+                isActive("/admin/contacts")
+                  ? "bg-gradient-to-r from-cyan-400 to-blue-600 text-white py-2 px-4 rounded-lg shadow-lg"
+                  : "bg-gray-200 text-black py-2 px-4 rounded-lg hover:bg-gray-300"
+              }
+            >
+              Admin (contacts)
+            </Link>
           </li>
         </ul>
       </nav>
-      <button
-        onClick={toggleDarkMode}
-        className={`fixed top-5 right-5 py-2 px-4 rounded-lg shadow-lg transition duration-300 ease-in-out ${
-          darkMode
-            ? 'bg-gradient-to-r from-cyan-400 to-blue-600 text-white'
-            : 'bg-gradient-to-r from-gray-200 to-gray-300 text-black'
-        }`}
-      >
-        {darkMode ? 'Mode Lumineux' : 'Mode Sombre'}
-      </button>
 
       <Component {...pageProps} />
     </SessionContextProvider>
