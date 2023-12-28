efc42c8fe36d9f1fcf2dac3ece7666f183040a9a
riribagz <bagot.ryan26@gmail.com>
2023-12-28 10:54:32 +0400

    Feat: add some style on post page

--- client/pages/post.jsx
+++ client/pages/post.jsx
@@ -54,10 +54,12 @@ export default function post() {
           <br />
         </>
       )}
-      <SearchBarPost className="" onSearch={setSearchTerm} />
-      <div className="flex justify-center text-center mt-10 relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table className="w-full text-sm text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+    <div className="flex justify-center mt-20">
+      <SearchBarPost onSearch={setSearchTerm} />
+    </div>
+     <div className="flex justify-center text-center mt-20 relative overflow-x-auto shadow-md sm:rounded-lg w-1/2 mx-auto">
+        <table className="w-full text-sm text-gray-500 dark:text-gray-400 table-white">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-white dark:text-gray-400">          
             <tr>
               <th scope="col" className="px-6 py-3">
                 Titre
