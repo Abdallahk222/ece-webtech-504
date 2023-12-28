ed7dfbd1809ff36ab299bf1e1c41af0a0e3385fe
riribagz <bagot.ryan26@gmail.com>
2023-12-28 11:06:33 +0400

    Feat: add styles on admin page

--- client/pages/admin/contacts.jsx
+++ client/pages/admin/contacts.jsx
@@ -4,6 +4,7 @@ import { useSupabaseClient } from "@supabase/auth-helpers-react";
 export default function Contacts() {
   const supabaseClient = useSupabaseClient();
   const [contacts, setContacts] = useState([]);
+
   useEffect(() => {
     async function fetchContacts() {
       const { data: contacts, error } = await supabaseClient
@@ -16,50 +17,41 @@ export default function Contacts() {
   }, []);
 
   return (
-    <div className="flex justify-center mt-10">
-      <br></br>
-      <table className="w-full text-sm text-center rtl:text-right text-gray-500 dark:text-gray-400">
-        <thead className="text-blue-500">
-          <tr>
-            <th scope="col" class="px-6 py-3">
+    <div className="flex justify-center text-center mt-20 relative overflow-x-auto shadow-md sm:rounded-lg w-3/4 mx-auto">
+    <table className="w-full text-sm text-gray-500 dark:text-gray-400">
+      <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-white dark:text-gray-400">
+        <tr>
+          <th scope="col" className="px-6 py-3">
               Id
             </th>
-            <th scope="col" class="px-6 py-3">
+            <th scope="col" className="px-6 py-3">
               Subject
             </th>
-            <th scope="col" class="px-6 py-3">
+            <th scope="col" className="px-6 py-3">
               Last Name
             </th>
-            <th scope="col" class="px-6 py-3">
+            <th scope="col" className="px-6 py-3">
               First Name
             </th>
-            <th scope="col" class="px-6 py-3">
+            <th scope="col" className="px-6 py-3">
               Email
             </th>
-            <th scope="col" class="px-6 py-3">
+            <th scope="col" className="px-6 py-3">
               Message
             </th>
           </tr>
         </thead>
         <tbody>
-          {contacts.map((contacts) => (
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <th
-                scope="row"
-                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                {contacts.id}
-              </th>
-              <th
-                scope="row"
-                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                {contacts.subject}
-              </th>
-              <td class="px-6 py-4">{contacts.lastname}</td>
-              <td class="px-6 py-4">{contacts.firstname}</td>
-              <td class="px-6 py-4">{contacts.email}</td>
-              <td class="px-6 py-4">{contacts.message}</td>
+          {contacts.map((contact) => (
+            <tr key={contact.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <td className="px-6 py-4 font-medium text-gray-900 dark:text-white">
+                {contact.id}
+              </td>
+              <td className="px-6 py-4">{contact.subject}</td>
+              <td className="px-6 py-4">{contact.lastname}</td>
+              <td className="px-6 py-4">{contact.firstname}</td>
+              <td className="px-6 py-4">{contact.email}</td>
+              <td className="px-6 py-4">{contact.message}</td>
             </tr>
           ))}
         </tbody>
