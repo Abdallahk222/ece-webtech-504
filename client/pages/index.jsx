f62f9ff0c6946fa580c7971046f3cfb5f954488b
hamed bamba <medyybamba@gmail.com>
2023-12-29 13:17:22 +0100

    fix: index page

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import SearchBar from "../components/SearchBar";
+import SearchBar from "../components/searchBar";
 import Movie from "../components/movie";
 
 const HomePage = () => {
